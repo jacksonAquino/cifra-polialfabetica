57680d4e7f0598b53caf2cc211d4b7d192391b98
João Araújo <j.henr@outlook.com>
2022-10-16 21:19:23 -0300

    fix: cifra monoalfabetica

--- monoalfabetica/script.js
+++ monoalfabetica/script.js
@@ -1,88 +1,23 @@
-const dictionary = {
-  // LOWER CASE
-  a: "q",
-  b: "w",
-  c: "e",
-  d: "r",
-  e: "t",
-  f: "y",
-  g: "u",
-  h: "i",
-  i: "o",
-  j: "p",
-  k: "a",
-  l: "s",
-  m: "d",
-  n: "f",
-  o: "g",
-  p: "h",
-  q: "j",
-  r: "k",
-  s: "l",
-  t: "z",
-  u: "x",
-  v: "c",
-  w: "v",
-  x: "b",
-  y: "n",
-  z: "m",
+const baseDictionary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-  //UPPER CASE
-  A: "Q",
-  B: "W",
-  C: "E",
-  D: "R",
-  E: "T",
-  F: "Y",
-  G: "U",
-  H: "I",
-  I: "O",
-  J: "P",
-  K: "A",
-  L: "S",
-  M: "D",
-  N: "F",
-  O: "G",
-  P: "H",
-  Q: "J",
-  R: "K",
-  S: "L",
-  T: "Z",
-  U: "X",
-  V: "C",
-  W: "V",
-  X: "B",
-  Y: "N",
-  Z: "M",
+const keys = baseDictionary.slice().sort((a, b) => 0.5 - Math.random())
 
-  //NUMBERS
-  0: "7",
-  1: "9",
-  2: "3",
-  3: "1",
-  4: "2",
-  5: "0",
-  6: "4",
-  7: "5",
-  8: "6",
-  9: "8",
-};
+console.log(baseDictionary, keys)
 
 const result_encript = document.getElementById('result_encript')
 
 const encryptBtn = document.getElementById('encryptBtn')
 const decryptBtn = document.getElementById('decryptBtn')
 
-
 encryptBtn.addEventListener('click', e => {
   e.preventDefault()
   const text_to_work = document.getElementById('word_to_work').value
 
-  if(!text_to_work){
+  if (!text_to_work) {
     alert('preencha os valores corretamente')
   } else {
-      const result = encrypt(text_to_work)
-      result_encript.innerText = result
+    const result = encrypt(text_to_work)
+    result_encript.innerText = result
   }
 })
 
@@ -90,20 +25,22 @@ decryptBtn.addEventListener('click', e => {
   e.preventDefault()
   const text_to_work = document.getElementById('word_to_work').value
 
-  if(!text_to_work){
+  if (!text_to_work) {
     alert('preencha os valores corretamente')
   } else {
-      const result = decrypt(text_to_work)
-      result_encript.innerText = result
+    const result = decrypt(text_to_work)
+    result_encript.innerText = result
   }
 })
 
 const encrypt = (input) => {
-  const dictKeys = Object.keys(dictionary);
   let encryptedString = "";
+
   for (let i = 0; i < input.length; i++) {
-    if (dictKeys.includes(input[i])) {
-      encryptedString += dictionary[input[i]];
+    const index = baseDictionary.findIndex((o) => o === input[i])
+    if (index !== -1) {
+      const newKey = keys[index]
+      encryptedString += newKey;
     } else {
       encryptedString += input[i];
     }
@@ -112,14 +49,13 @@ const encrypt = (input) => {
 };
 
 const decrypt = (input) => {
-  const dictValues = Object.values(dictionary);
-  const dictEntries = Object.entries(dictionary);
   let decryptedString = "";
 
   for (let i = 0; i < input.length; i++) {
-    if (dictValues.includes(input[i])) {
-      const key = dictEntries.find((entry) => entry[1] === input[i]);
-      decryptedString += key[0];
+    const index = keys.findIndex((o) => o === input[i]);
+    if (index !== -1) {
+      const decryptedKey = baseDictionary[index]
+      decryptedString += decryptedKey;
     } else {
       decryptedString += input[i];
     }
